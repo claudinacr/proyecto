7b9d905f74246685d57f7e11a2531754f8afe89c
claudinacr <claudinacr@gmail.com>
2017-09-26 11:26:13 -0300

    primera version

---
+++ P_W/app.js
@@ -0,0 +1,6 @@
+alert("Bienvenidos a Nuestra Pagina Web");
+var mostrarMensaje = true;
+
+if(mostrarMensaje) {
+ alert("Hola Mundo");
+}
---
+++ P_W_E/app.js
@@ -0,0 +1,3 @@
+/*1. Hacer un programa que al dar la hora hh, mm, ss, 
+muestre las horas, minutos y segundos y también 
+nos calcule la hora después de 2 segundos */
